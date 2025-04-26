075a74ecf9437ade803a87fde918073afc677575
GaganNk127 <143861632+GaganNk127@users.noreply.github.com>
2025-04-26 18:39:18 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,7 @@ import projectOne from "../assets/images/project-1.png";
 import projectTwo from "../assets/images/project-2.png";
 import projectThree from "../assets/images/project-3.png";
 import mentalmilestone from "../assets/images/mentalmilestone.png";
+import susta from "../assets/images/susta.png"
 import Se from "../assets/images/Se.png"; 
 import Speecho from "../assets/images/Speecho.png";
 import { Pagination, Autoplay } from "swiper";
@@ -30,6 +31,11 @@ const Project = () => {
       github_link: "https://github.com/GaganNk127/milestones",
      // live_link: "https://highking01.netlify.app",
     },
+    {
+      img: susta,
+      name : "Sustainable Scanner",
+      github_link: "https://github.com/GaganNk127/Sustainableproductscanner",
+    }
     // {
     //   img: projectFour,
     //   name: "Quizzify",
